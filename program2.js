24240566eb8da77ccdf70fe187216753bd3329bd
VivekPatidar <Vivekpatidar0190@gmail.com>
2024-04-26 13:32:32 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -9,8 +9,7 @@ function longestSubstring(s) {
     for (let end = 0; end < s.length; end++) {
         const currentChar = s[end];
 
-        // If the current character is already in the map and its index is greater than or equal to the start index,
-        // update the start index to the index after the last occurrence of the character
+       
         if (charMap.has(currentChar) && charMap.get(currentChar) >= start) {
             start = charMap.get(currentChar) + 1;
         }
